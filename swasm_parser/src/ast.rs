5f1566068ec3eb6e5bfa3a46757c897594cc70cb
Rodrigo C. Gryzinski <rogryza@gmail.com>
2017-10-08 20:21:41 -0300

    askldaklsm

--- swasm_parser/src/ast.rs
+++ swasm_parser/src/ast.rs
@@ -1,13 +1,60 @@
 #[derive(Clone, Debug, PartialEq)]
-pub enum SExpr {
-    Quote,
-    If,
+pub struct Program(pub Vec<Form>);
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Form{
+    Def(Def),
+    Expr(Expr),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Def {
+    Var(VarDef),
+    Begin(Vec<Def>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum VarDef {
+    Var(String, Body),
+    Fun(Vec<String>, Option<String>, Body),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Body {
+    definitions: Vec<Def>,
+    expressions: Vec<Expr>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Expr {
+    Const(Const),
+    Var(String),
+    Quote(Datum),
+    Lambda(Args, Body),
+    If(Box<Expr>, Box<Expr>, Option<Expr>),
+    Set(String, Box<Expr>),
+    Call(Box<Expr>, Vec<Expr>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Args {
+    Single(String),
+    Multi(Vec<String>, Option<String>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Const {
     Bool(bool),
     Number(Number),
     String(String),
-    Ident(String),
-    List(Vec<SExpr>),
-    DottedList(Vec<SExpr>, Box<SExpr>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Datum {
+    Bool(bool),
+    Number(Number),
+    String(String),
+    List(Vec<Datum>, Option<Box<Datum>>),
 }
 
 #[derive(Clone, Debug, PartialEq, PartialOrd)]
